4f7049279c84469ab117c677751708fd0db73208
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-30 20:34:17 -0300

    Create TransactionServices

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,42 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  category_id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category_id,
+    title,
+    value,
+    type,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const balance = await transactionsRepository.getBalance();
+
+    const sourceLessBalance = type === 'outcome' && balance.total < value;
+
+    if (sourceLessBalance) {
+      throw new Error('Source account not have balance for this operation');
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
