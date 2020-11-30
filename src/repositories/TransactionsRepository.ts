11c4aef5bc20b54cf630dbd2d18546b27d086e25
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-30 20:33:55 -0300

    Add method getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,22 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((acc, credit) => acc + credit.value, 0);
+    const outcome = transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((acc, debit) => acc + debit.value, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 }
 
